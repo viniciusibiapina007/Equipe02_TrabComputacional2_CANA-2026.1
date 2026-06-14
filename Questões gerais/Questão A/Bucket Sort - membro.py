5fb18731e4a8f432025383c876ae55dbbc78cfd0
viniciusibiapina007 <viniciusaluno2020@gmail.com>
2026-06-14 18:30:38 -0300

    Update Bucket Sort - membro.py

--- Questões gerais/Questão A/Bucket Sort - membro.py
+++ Questões gerais/Questão A/Bucket Sort - membro.py
@@ -1,43 +1,26 @@
+import matplotlib.pyplot as plt
 import random
 import timeit
 
-def insertion_sort(bld):
-    """Ordena cada balde individualmente."""
-    for i in range(1, len(bld)):
-        key = bld[i]
-        j = i - 1
-        while j >= 0 and bld[j] > key:
-            bld[j + 1] = bld[j]
-            j -= 1
-        bld[j + 1] = key
-    return bld
 
+def shell_sort(arr):
+    n = len(arr)
+    meio = n // 2
 
-def bucket_sort(arr):
-    if not arr:
-        return arr
+    while meio > 0:
+        for i in range(meio, n):
+            temp = arr[i]
+            j = i
 
-    # 1. Criação dos baldes ========================================
-    qtd_balde = len(arr)
-    max_val = max(arr)
-    min_val = min(arr)
+            while j >= meio and arr[j - meio] > temp:
+                arr[j] = arr[j - meio]
+                j -= meio
 
-    # Range de valores por balde
-    faixa_balde = (max_val - min_val) / qtd_balde + 1
+            arr[j] = temp
 
-    baldes = [[] for _ in range(qtd_balde)]
+        meio //= 2
 
-    # 2. Distribuição dos elementos nos baldes ======================
-    for num in arr:
-        index = int((num - min_val) / faixa_balde)
-        baldes[index].append(num)
-
-    # 3. Ordenação de cada balde e concatenação ======================
-    array_ordenado = []
-    for balde in baldes:
-        array_ordenado.extend(insertion_sort(balde))
-
-    return array_ordenado
+    return arr
 
 
 def geraLista(tam):
@@ -56,8 +39,4 @@ tempos = []
 
 for t in tamanhos:
     lista = geraLista(t)
-    tempos.append(timeit.timeit(f'bucket_sort({lista.copy()})', setup='from __main__ import bucket_sort', number=1))
-
-    print(sum(lista))
-
-print(tempos)
+    tempos.append(timeit.timeit(f'shell_sort({lista.copy()})', setup='from __main__ import shell_sort', number=1))
