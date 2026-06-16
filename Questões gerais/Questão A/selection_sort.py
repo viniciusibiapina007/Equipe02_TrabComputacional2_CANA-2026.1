2e946bfe0015a038ca42a0c2fa835194ae3c7f1a
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-06-15 21:09:11 -0300

    Refactor selection_sort.py to remove unused code
    Removed random list generation and timing code.

--- Questões gerais/Questão A/selection_sort.py
+++ Questões gerais/Questão A/selection_sort.py
@@ -1,5 +1,4 @@
-import random #David Lucas
-import time
+#David Lucas
 
 def selectionSort(lista):
     for i in range(len(lista)):
@@ -9,20 +8,3 @@ def selectionSort(lista):
                 menor = j
         lista[i], lista[menor] = lista[menor], lista[i]
     return lista
-
-tamanhos = [1000, 3000, 6000, 9000, 12000, 15000, 18000, 21000, 24000]
-
-for tamanho in tamanhos:
-    l = []
-    for i in range(tamanho):
-        l.append(random.random())
-
-    tempo_inicial = time.time()
-
-    selectionSort(l)
-
-    tempo_final = time.time()
-
-    tempo_total = tempo_final - tempo_inicial
-
-    print(f"Tempo de execução: {tempo_total:.4f} segundos")
