f7219140709d17fe48a390192c8be59116b50439
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-06-15 21:06:50 -0300

    Refactor merge_sort.py by removing unused code
    Removed unused imports and main function for cleaner code.

--- Questões gerais/Questão A/merge_sort.py
+++ Questões gerais/Questão A/merge_sort.py
@@ -1,6 +1,4 @@
-import random as rd #João Igor
-
-t = [1000, 3000, 6000, 9000, 12000, 15000, 18000, 21000, 24000]
+#João Igor
 
 def mergeSort(arr, p, r):
       if p < r:
@@ -45,56 +43,3 @@ def merge(arr, p, q, r):
           arr[k] = R[j]
           j += 1
           k += 1
-
-def preencherArr(arr, n):
-      for i in range(n):
-          arr.append(rd.randint(0, 25000))
-      return arr
-
-def main():
-    op = int(input("1- 1000\n2- 3000\n3- 6000\n4- 9000\n5- 12000\n6- 15000\n7- 18000\n8- 21000\n9- 24000\n10- teste\n0- sair\nInforme uma opção de tamanho: ")) #OBS: apenas implementar melhor visão de que a ordenação foi feita
-
-    while op != 0:
-
-        if op == 1:
-            n = t[0]
-
-        elif op == 2:
-            n = t[1]
-
-        elif op == 3:
-             n = t[2]
-
-        elif op == 4:
-            n = t[3]
-
-        elif op == 5:
-            n = t[4]
-
-        elif op == 6:
-            n = t[5]
-
-        elif op == 7:
-            n = t[6]
-
-        elif op == 8:
-            n = t[7]
-
-        elif op == 9:
-            n = t[8]
-
-        elif op == 10:
-            n = 10
-
-        else:
-            n = 0
-
-        if n > 0:
-            arr = []
-            arr = preencherArr(arr, n)
-            mergeSort(arr, 0, n - 1)
-            print(arr)
-
-        op = int(input("1- 1000\n2- 3000\n3- 6000\n4- 9000\n5- 12000\n6- 15000\n7- 18000\n8- 21000\n9- 24000\n10- teste\n0- sair\nInforme uma opção de tamanho: "))
-
-main()
